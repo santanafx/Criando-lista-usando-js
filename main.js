790ca7eaa5d5c9ce179da4dc1817249c2a83a773
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-01-01 17:34:28 -0300

    Lista Adicionando nome, idade e sexo

--- main.js
+++ main.js
@@ -6,20 +6,15 @@ form.addEventListener("submit", (evento) => {
   var nome = evento.target.elements["nome"].value;
   var idade = evento.target.elements["idade"].value;
   var sexo = evento.target.elements["sexo"].value;
-  criaElemento(nome, idade, sexo);
+  criaElemento(nome);
+  criaElemento(idade);
+  criaElemento(sexo);
 });
 
-var criaElemento = (nome, idade, sexo) => {
+var criaElemento = (x) => {
   var novaLi = document.createElement("li");
   novaLi.classList.add("item"); //adiciona uma classe chama item ao elemento li
   var lista = document.querySelector(".lista");
   lista.appendChild(novaLi);
-
-  var novoP = document.createElement("p");
-  novoP.innerHTML += nome;
-  novaLi.appendChild(novoP);
-  novoP.innerHTML += idade;
-  novaLi.appendChild(novoP);
-  novoP.innerHTML += sexo;
-  novaLi.appendChild(novoP);
+  novaLi.innerHTML += x;
 };
