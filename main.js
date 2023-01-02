6c76ee582f2086df705928f2d38902af0b59ba9b
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-01-02 09:25:55 -0300

    Salvando arquivos no localStorage

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var form = document.querySelector(".formulario");
+var array = []; //cria um array vázio
 
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
@@ -6,17 +7,34 @@ form.addEventListener("submit", (evento) => {
   var nome = evento.target.elements["nome"].value;
   var idade = evento.target.elements["idade"].value;
   var sexo = evento.target.elements["sexo"].value;
-  criaElemento(nome);
-  criaElemento(idade);
-  criaElemento(sexo);
+
+  criaElemento(nome, idade, sexo);
 });
 
-var criaElemento = (x) => {
-  var novaLi = document.createElement("li");
-  novaLi.classList.add("item"); //adiciona uma classe chama item ao elemento li
+var criaElemento = (nome, idade, sexo) => {
+  var novaLi1 = document.createElement("li");
+  novaLi1.classList.add("item"); //adiciona uma classe chama item ao elemento li
+  var novaLi2 = document.createElement("li");
+  novaLi2.classList.add("item"); //adiciona uma classe chama item ao elemento li
+  var novaLi3 = document.createElement("li");
+  novaLi3.classList.add("item"); //adiciona uma classe chama item ao elemento li
   var lista = document.querySelector(".lista");
-  lista.appendChild(novaLi);
-  novaLi.innerHTML += x;
+  lista.appendChild(novaLi1);
+  novaLi1.innerHTML += nome;
+  lista.appendChild(novaLi2);
+  novaLi2.innerHTML += idade;
+  lista.appendChild(novaLi3);
+  novaLi3.innerHTML += sexo;
+
+  var objeto = {
+    nome: nome,
+    idade: idade,
+    sexo: sexo,
+  };
+
+  array.push(objeto); //comando .push joga o objeto dentro do array
+
+  localStorage.setItem("conjunto", JSON.stringify(array)); //comando JSON.stringify serve para
 };
 
 //SALVAR VARIÁVEIS NO LocalStorage
@@ -25,3 +43,4 @@ var criaElemento = (x) => {
 //localStorage.getItem("chave") trás a string "valor" pois o comando getItem busca o valor da chave
 //localStorage.removeItem("chave") deleta a informação
 //localSorage.clear() deleta tudo
+//para verificar os dados salvos no localStorage navegar até Application
